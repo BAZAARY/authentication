a3b25f58e979c1d74449f348bcdef208800d37ba
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-10-21 18:21:32 -0500

    roll back

--- src/configs/databaseConfig.js
+++ src/configs/databaseConfig.js
@@ -5,6 +5,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Credenciales supabase
 export const supabaseUrl = "https://woloirkcggmgeyfzbylf.supabase.co";
-export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbG9pcmtjZ2dtZ2V5ZnpieWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY3NDI3MTYsImV4cCI6MjAxMjMxODcxNn0.-0zicIyNb-e2j5QAje1wrpDf2wGpedLGrJsVp7iMD4Q";
+export const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
